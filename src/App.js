d8176f9a2712c36de49201ec7da125598be93d13
KAYA jordan <kayajordan21@gmail.com>
2022-08-11 13:07:37 +0200

    ajout de navigation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import Footer from "./components/Footer/Footer";
 import NavBar from "./components/NavBar/NavBar";
 import AddBooks from "./containers/AddBooks/AddBooks";
+import SearchBooks from "./containers/SeachBooks/SearchBooks";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom"
+
 
 
 function App() {
   return (
-    <div className="App">
-      <NavBar/>
-        <AddBooks/>
-      <Footer/>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<AddBooks />} />
+        <Route  path="/search" element={<SearchBooks />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
